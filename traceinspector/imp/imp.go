62ccf54a170af2daabea10ab8fc449424bdd1514
Dashadower <tttllshin@gmail.com>
2026-04-04 18:45:35 +0900

    Expose fields in imp

--- traceinspector/imp/imp.go
+++ traceinspector/imp/imp.go
@@ -1,5 +1,10 @@
 package imp
 
+import (
+	"fmt"
+	"strings"
+)
+
 ////////////
 // Type declarations
 
@@ -8,15 +13,38 @@ type ImpTypes interface {
 }
 
 type IntType ImpTypes
+
 type BoolType ImpTypes
 type NonType ImpTypes
 
 type ImpArray struct {
-	element_type ImpTypes
-	len          int
+	Element_type ImpTypes
+	Len          int
+}
+
+func (ty ImpArray) String() string {
+	return fmt.Sprintf("ArrayType<%s>", ty.Element_type)
 }
 
 func (ty ImpArray) isType()
+
+// Given a Value and a specified Type, check that the Value is of the specified type
+func check_val_type_match(val ImpValues, _type ImpTypes) bool {
+	switch val_ty := val.(type) {
+	case *IntVal:
+		_, type_is_int := _type.(IntType)
+		return type_is_int
+	case *BoolVal:
+		_, type_is_bool := _type.(BoolType)
+		return type_is_bool
+	case *ArrayVal:
+		array_ty, type_is_array := _type.(ImpArray)
+		return type_is_array && array_ty.Element_type == val_ty.element_type
+	}
+	return false
+}
+
+////////////////////////
 
 type ImpValues interface {
 	isValue()
@@ -26,10 +54,18 @@ type IntVal struct {
 	val int
 }
 
+func (val IntVal) String() string {
+	return val.String()
+}
+
 func (*IntVal) isValue() {}
 
 type BoolVal struct {
 	val bool
+}
+
+func (val BoolVal) String() string {
+	return val.String()
 }
 
 func (*BoolVal) isValue() {}
@@ -39,14 +75,45 @@ type ArrayVal struct {
 	val          []ImpValues
 }
 
+func (val ArrayVal) String() string {
+	return val.String()
+}
+
 func (*ArrayVal) isValue() {}
 
+type StringVal struct {
+	val string
+}
+
+func (val StringVal) String() string {
+	return val.val
+}
+
+func (*StringVal) isValue() {}
+
 type NoneVal struct{}
+
+func (val NoneVal) String() string {
+	return "NoneVal"
+}
 
 func (*NoneVal) isValue() {}
 
 //////////////////////
+// Function definitions
 
+type ArgPair struct {
+	name     string
+	arg_type ImpTypes
+}
+
+type ImpFunction struct {
+	Arg_names   []ArgPair
+	Body        []Stmt
+	Return_type ImpTypes
+}
+
+// ////////////////////
 type Node struct {
 	Line_num int // return the line number corresponding to the original source
 }
@@ -57,11 +124,6 @@ type Expr interface {
 
 type Stmt interface {
 	isStmt()
-}
-
-type ArgPair struct {
-	name string
-	expr Expr
 }
 
 // expressions
@@ -78,18 +140,26 @@ func (expr VarExpr) String() string {
 	return expr.name
 }
 
-type IntValueExpr struct {
+type IntLitExpr struct {
 	value int
 	Node
 }
 
-func (*IntValueExpr) isExpr() {}
+func (*IntLitExpr) isExpr() {}
 
-type BoolValueExpr struct {
+func (expr IntLitExpr) String() string {
+	return fmt.Sprintf("%d", expr.value)
+}
+
+type BoolLitExpr struct {
 	value bool
 }
 
-func (*BoolValueExpr) isExpr() {}
+func (*BoolLitExpr) isExpr() {}
+
+func (expr BoolLitExpr) String() string {
+	return fmt.Sprintf("%t", expr.value)
+}
 
 type AddExpr struct {
 	lhs, rhs Expr
@@ -97,11 +167,19 @@ type AddExpr struct {
 
 func (*AddExpr) isExpr() {}
 
+func (expr AddExpr) String() string {
+	return fmt.Sprintf("%s + %s", expr.lhs, expr.rhs)
+}
+
 type SubExpr struct {
 	lhs, rhs Expr
 }
 
 func (*SubExpr) isExpr() {}
+
+func (expr SubExpr) String() string {
+	return fmt.Sprintf("%s - %s", expr.lhs, expr.rhs)
+}
 
 type MulExpr struct {
 	lhs, rhs Expr
@@ -109,11 +187,29 @@ type MulExpr struct {
 
 func (*MulExpr) isExpr() {}
 
+func (expr MulExpr) String() string {
+	return fmt.Sprintf("%s * %s", expr.lhs, expr.rhs)
+}
+
 type DivExpr struct {
 	lhs, rhs Expr
 }
 
 func (*DivExpr) isExpr() {}
+
+func (expr DivExpr) String() string {
+	return fmt.Sprintf("%s / %s", expr.lhs, expr.rhs)
+}
+
+type ParenExpr struct {
+	subexpr Expr
+}
+
+func (*ParenExpr) isExpr() {}
+
+func (expr ParenExpr) String() string {
+	return fmt.Sprintf("(%s)", expr.subexpr)
+}
 
 type ArrayIndexExpr struct {
 	base  Expr
@@ -122,11 +218,19 @@ type ArrayIndexExpr struct {
 
 func (*ArrayIndexExpr) isExpr() {}
 
+func (expr ArrayIndexExpr) String() string {
+	return fmt.Sprintf("%s[%s]", expr.base, expr.index)
+}
+
 type EqExpr struct {
 	lhs, rhs Expr
 }
 
 func (*EqExpr) isExpr() {}
+
+func (expr EqExpr) String() string {
+	return fmt.Sprintf("%s == %s", expr.lhs, expr.rhs)
+}
 
 type NeqExpr struct {
 	lhs, rhs Expr
@@ -134,11 +238,19 @@ type NeqExpr struct {
 
 func (*NeqExpr) isExpr() {}
 
+func (expr NeqExpr) String() string {
+	return fmt.Sprintf("%s != %s", expr.lhs, expr.rhs)
+}
+
 type NotExpr struct {
 	subexpr Expr
 }
 
 func (*NotExpr) isExpr() {}
+
+func (expr NotExpr) String() string {
+	return fmt.Sprintf("!%s", expr.subexpr)
+}
 
 type AndExpr struct {
 	lhs, rhs Expr
@@ -146,26 +258,36 @@ type AndExpr struct {
 
 func (*AndExpr) isExpr() {}
 
+func (expr AndExpr) String() string {
+	return fmt.Sprintf("%s && %s", expr.lhs, expr.rhs)
+}
+
 type OrExpr struct {
 	lhs, rhs Expr
 }
 
 func (*OrExpr) isExpr() {}
 
+func (expr OrExpr) String() string {
+	return fmt.Sprintf("%s || %s", expr.lhs, expr.rhs)
+}
+
 type CallExpr struct {
 	func_name string
-	args      []ArgPair
+	args      []Expr
 }
 
 func (*CallExpr) isExpr() {}
 
-// statements
-
-type SeqStmt struct {
-	lhs, rhs Stmt
+func (expr CallExpr) String() string {
+	var func_args []string
+	for _, arg := range expr.args {
+		func_args = append(func_args, fmt.Sprintf("%s", arg))
+	}
+	return fmt.Sprintf("%s(%s)", expr.func_name, strings.Join(func_args, ", "))
 }
 
-func (*SeqStmt) isStmt() {}
+// statements
 
 type SkipStmt struct {
 }
@@ -179,36 +301,80 @@ type AssignStmt struct {
 
 func (*AssignStmt) isStmt() {}
 
-type InputStmt struct {
-	assign_var VarExpr
+func (stmt AssignStmt) String() string {
+	return fmt.Sprintf("%s = %s", stmt.lhs, stmt.rhs)
 }
-
-func (*InputStmt) isStmt() {}
 
 type IfElseStmt struct {
 	cond       Expr
-	true_stmt  Stmt
-	false_stmt Stmt
+	true_stmt  []Stmt
+	false_stmt []Stmt
 }
 
 func (*IfElseStmt) isStmt() {}
 
+func (stmt IfElseStmt) String() string {
+	var true_stmts []string
+	var false_stmts []string
+	for _, stmt := range stmt.true_stmt {
+		true_stmts = append(true_stmts, fmt.Sprintf("    %s", stmt))
+	}
+	for _, stmt := range stmt.false_stmt {
+		false_stmts = append(false_stmts, fmt.Sprintf("    %s", stmt))
+	}
+	return fmt.Sprintf("if %s {\n%s} else {\n%s}\n", stmt.cond, strings.Join(true_stmts, "\n"), strings.Join(false_stmts, "\n"))
+}
+
 type WhileStmt struct {
 	cond      Expr
-	body_stmt Stmt
+	body_stmt []Stmt
 }
 
 func (*WhileStmt) isStmt() {}
 
+func (stmt WhileStmt) String() string {
+	var substmts []string
+	for _, stmt := range stmt.body_stmt {
+		substmts = append(substmts, fmt.Sprintf("    %s", stmt))
+	}
+	return fmt.Sprintf("while %s {\n%s}\n", stmt.cond, strings.Join(substmts, "\n"))
+}
+
 type CallStmt struct {
 	func_name string
-	args      []ArgPair
+	args      []Expr
 }
 
 func (*CallStmt) isStmt() {}
+
+func (stmt CallStmt) String() string {
+	var args []string
+	for _, arg := range stmt.args {
+		args = append(args, fmt.Sprintf("    %s", arg))
+	}
+	return fmt.Sprintf("%s(%s)\n", stmt.func_name, strings.Join(args, ", "))
+}
+
+type PrintStmt struct {
+	args []Expr
+}
+
+func (*PrintStmt) isStmt() {}
+
+func (stmt PrintStmt) String() string {
+	var args []string
+	for _, arg := range stmt.args {
+		args = append(args, fmt.Sprintf("    %s", arg))
+	}
+	return fmt.Sprintf("print(%s)\n", strings.Join(args, ", "))
+}
 
 type ReturnStmt struct {
 	arg Expr
 }
 
 func (*ReturnStmt) isStmt() {}
+
+func (stmt ReturnStmt) String() string {
+	return fmt.Sprintf("return %s\n", stmt.arg)
+}
--- traceinspector/imp/interpreter.go
+++ traceinspector/imp/interpreter.go
@@ -1,31 +1,46 @@
 package imp
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type ImpState struct {
-	vars         map[string]ImpValues // all exprs are reduced to go values
-	return_value ImpValues            // The return value of the current local scope, if exists
+	vars                  map[string]ImpValues // all exprs are reduced to go values
+	current_function_name string
+	return_value          ImpValues // The return value of the current local scope, if exists
 }
 
 type ImpInterpreter struct {
-	states    []*ImpState // a stack of program states
-	functions map[string]*Stmt
+	States    []*ImpState // a stack of program states to represent scopes
+	Functions map[string]ImpFunction
 }
 
 func (interpreter *ImpInterpreter) get_top_state() *ImpState {
-	return interpreter.states[len(interpreter.states)-1]
+	return interpreter.States[len(interpreter.States)-1]
+}
+
+// Return the topmost variable name and bool indicating if the variable exists
+func (interpreter *ImpInterpreter) get_variable(name string) (ImpValues, bool) {
+	for stack_index := len(interpreter.States) - 1; stack_index >= 0; stack_index-- {
+		var_value, var_exists := interpreter.States[stack_index].vars[name]
+		if var_exists {
+			return var_value, true
+		}
+	}
+	return nil, false
 }
 
 func (interpreter *ImpInterpreter) push_state(state ImpState) {
-	interpreter.states = append(interpreter.states, &state)
+	interpreter.States = append(interpreter.States, &state)
 }
 
 func (interpreter *ImpInterpreter) pop_state() {
-	interpreter.states = interpreter.states[:len(interpreter.states)-1]
+	interpreter.States = interpreter.States[:len(interpreter.States)-1]
 }
 
 func (interpreter *ImpInterpreter) eval_VarExpr(node VarExpr) ImpValues {
-	var_value, var_exists := interpreter.get_top_state().vars[node.name]
+	var_value, var_exists := interpreter.get_variable(node.name)
 	if !var_exists {
 		panic("Unknown variable " + node.name)
 	}
@@ -33,13 +48,46 @@ func (interpreter *ImpInterpreter) eval_VarExpr(node VarExpr) ImpValues {
 }
 
 func (interpreter *ImpInterpreter) eval_Expr(node Expr) ImpValues {
-
+	switch node_ty := node.(type) {
+	case *VarExpr:
+		return interpreter.eval_VarExpr(*node_ty)
+	case *IntLitExpr:
+		return interpreter.eval_IntLitExpr(*node_ty)
+	case *BoolLitExpr:
+		return interpreter.eval_BoolLitExpr(*node_ty)
+	case *AddExpr:
+		return interpreter.eval_AddExpr(*node_ty)
+	case *SubExpr:
+		return interpreter.eval_SubExpr(*node_ty)
+	case *MulExpr:
+		return interpreter.eval_MulExpr(*node_ty)
+	case *DivExpr:
+		return interpreter.eval_DivExpr(*node_ty)
+	case *ParenExpr:
+		return interpreter.eval_ParenExpr(*node_ty)
+	case *ArrayIndexExpr:
+		return interpreter.eval_ArrayIndexExpr(*node_ty)
+	case *EqExpr:
+		return interpreter.eval_EqExpr(*node_ty)
+	case *NeqExpr:
+		return interpreter.eval_NeqExpr(*node_ty)
+	case *NotExpr:
+		return interpreter.eval_NotExpr(*node_ty)
+	case *AndExpr:
+		return interpreter.eval_AndExpr(*node_ty)
+	case *OrExpr:
+		return interpreter.eval_OrExpr(*node_ty)
+	case *CallExpr:
+		return interpreter.eval_CallExpr(*node_ty)
+	default:
+		panic(fmt.Sprintf("Unimplemented expr type %s", node))
+	}
 }
 
 func (interpreter *ImpInterpreter) eval_Expr_lvalue(lhs Expr, rhs ImpValues) ImpValues {
 	lhs_var, lhs_is_var := lhs.(*VarExpr)
 	if lhs_is_var {
-		_, lhs_exists := interpreter.get_top_state().vars[lhs_var.name]
+		_, lhs_exists := interpreter.get_variable(lhs_var.name)
 		if !lhs_exists {
 			switch rhs.(type) {
 			case *IntVal:
@@ -50,17 +98,18 @@ func (interpreter *ImpInterpreter) eval_Expr_lvalue(lhs Expr, rhs ImpValues) Imp
 				interpreter.get_top_state().vars[lhs_var.name] = &ArrayVal{}
 			}
 		}
-		return interpreter.get_top_state().vars[lhs_var.name]
+		var_val, _ := interpreter.get_variable(lhs_var.name)
+		return var_val
 	} else {
 		return interpreter.eval_Expr(lhs)
 	}
 }
 
-func (interpreter *ImpInterpreter) eval_IntValueExpr(node IntValueExpr) ImpValues {
+func (interpreter *ImpInterpreter) eval_IntLitExpr(node IntLitExpr) ImpValues {
 	return &IntVal{val: node.value}
 }
 
-func (interpreter *ImpInterpreter) eval_BoolValueExpr(node BoolValueExpr) ImpValues {
+func (interpreter *ImpInterpreter) eval_BoolLitExpr(node BoolLitExpr) ImpValues {
 	return &BoolVal{val: node.value}
 }
 
@@ -106,7 +155,7 @@ func (interpreter *ImpInterpreter) eval_MulExpr(node MulExpr) ImpValues {
 	return &IntVal{val: lhs_val.val * rhs_val.val}
 }
 
-func (interpreter *ImpInterpreter) eval_DivExpr(node AddExpr) ImpValues {
+func (interpreter *ImpInterpreter) eval_DivExpr(node DivExpr) ImpValues {
 	lhs_val, lhs_is_int := interpreter.eval_Expr(node.lhs).(*IntVal)
 	rhs_val, rhs_is_int := interpreter.eval_Expr(node.rhs).(*IntVal)
 
@@ -118,6 +167,10 @@ func (interpreter *ImpInterpreter) eval_DivExpr(node AddExpr) ImpValues {
 		panic(fmt.Sprintf("RHS of division should be an int value, but got '%s'", node.rhs))
 	}
 	return &IntVal{val: lhs_val.val / rhs_val.val}
+}
+
+func (interpreter *ImpInterpreter) eval_ParenExpr(node ParenExpr) ImpValues {
+	return interpreter.eval_Expr(node.subexpr)
 }
 
 func (interpreter *ImpInterpreter) eval_ArrayIndexExpr(node ArrayIndexExpr) ImpValues {
@@ -180,15 +233,34 @@ func (interpreter *ImpInterpreter) eval_OrExpr(node OrExpr) ImpValues {
 	return &BoolVal{val: lhs_val.val || rhs_val.val}
 }
 
-func (interpreter *ImpInterpreter) eval_CallExpr(node CallExpr) ImpValues {
+// Imp is pass-by-value for int/bool, but arrays are passed references
+func (interpreter *ImpInterpreter) eval_function_call(func_name string, args []Expr) ImpValues {
+	// copy values if primitive
+	prepare_args := func(arg ImpValues) ImpValues {
+		switch arg_ty := arg.(type) {
+		case *IntVal:
+			return &IntVal{val: arg_ty.val}
+		case *BoolVal:
+			return &BoolVal{val: arg_ty.val}
+		case *ArrayVal:
+			return arg_ty
+		}
+		panic(fmt.Sprintf("Function call: Unknown arg type %s", arg))
+	}
 	func_local_state := ImpState{vars: make(map[string]ImpValues)}
-	for _, arg := range node.args {
-		func_local_state.vars[arg.name] = interpreter.eval_Expr(arg.expr)
+	for index, arg_expr := range args {
+		arg_info := interpreter.Functions[func_name].Arg_names[index]
+		arg_val := prepare_args(interpreter.eval_Expr(arg_expr))
+		if !check_val_type_match(arg_val, arg_info.arg_type) {
+			panic(fmt.Sprintf("Argument '%s' of function '%s' is defined as type %s, but passed expr '%s' of type %s", arg_info.name, func_name, arg_info.arg_type, arg_expr, arg_val))
+		}
+		func_local_state.vars[arg_info.name] = arg_val
 	}
 	interpreter.push_state(func_local_state)
-	interpreter.eval_Stmt(*interpreter.functions[node.func_name])
+	interpreter.eval_Stmt(interpreter.Functions[func_name].Body)
 	return_value := interpreter.get_top_state().return_value
 	interpreter.pop_state()
+
 	if return_value == nil {
 		return &NoneVal{}
 	} else {
@@ -196,7 +268,11 @@ func (interpreter *ImpInterpreter) eval_CallExpr(node CallExpr) ImpValues {
 	}
 }
 
-func (interpreter *ImpInterpreter) eval_Skip(SkipStmt) {}
+func (interpreter *ImpInterpreter) eval_CallExpr(node CallExpr) ImpValues {
+	return interpreter.eval_function_call(node.func_name, node.args)
+}
+
+func (interpreter *ImpInterpreter) eval_SkipStmt(SkipStmt) {}
 
 func (interpreter *ImpInterpreter) eval_AssignStmt(node AssignStmt) {
 	rhs_val := interpreter.eval_Expr(node.rhs)
@@ -204,24 +280,92 @@ func (interpreter *ImpInterpreter) eval_AssignStmt(node AssignStmt) {
 	case *IntVal:
 		rhs_intval, rhs_is_intval := rhs_val.(*IntVal)
 		if !rhs_is_intval {
-			panic(fmt.Sprintf("Attempting to assign RHS '%s' of type %T to LHS '%s' of type %T", node.rhs, rhs_val, node.lhs, lhs_loc))
+			panic(fmt.Sprintf("Attempted to assign RHS '%s' of type %s to LHS '%s' of type %s", node.rhs, rhs_val, node.lhs, lhs_loc))
 		}
 		lhs_loc.val = rhs_intval.val
 	case *BoolVal:
 		rhs_intval, rhs_is_boolval := rhs_val.(*BoolVal)
 		if !rhs_is_boolval {
-			panic(fmt.Sprintf("Attempting to assign RHS '%s' of type %T to LHS '%s' of type %T", node.rhs, rhs_val, node.lhs, lhs_loc))
+			panic(fmt.Sprintf("Attempted to assign RHS '%s' of type %s to LHS '%s' of type %s", node.rhs, rhs_val, node.lhs, lhs_loc))
 		}
 		lhs_loc.val = rhs_intval.val
 	case *ArrayVal:
 		rhs_intval, rhs_is_arrayval := rhs_val.(*ArrayVal)
 		if !rhs_is_arrayval {
-			panic(fmt.Sprintf("Attempting to assign RHS '%s' of type %T to LHS '%s' of type %T", node.rhs, rhs_val, node.lhs, lhs_loc))
+			panic(fmt.Sprintf("Attempted to assign RHS '%s' of type %s to LHS '%s' of type %s", node.rhs, rhs_val, node.lhs, lhs_loc))
 		}
 		lhs_loc.val = rhs_intval.val
 	}
 }
 
-func (interpreter *ImpInterpreter) eval_Stmt(node Stmt) {
+func (interpreter *ImpInterpreter) eval_IfElseStmt(node IfElseStmt) {
+	cond_val := interpreter.eval_Expr(node.cond)
+	cond_boolval, cond_is_bool := cond_val.(*BoolVal)
+	if !cond_is_bool {
+		panic(fmt.Sprintf("If statement got non-boolean condition '%s'\n", node.cond))
+	}
+	if cond_boolval.val {
+		interpreter.eval_Stmt(node.true_stmt)
+	} else {
+		interpreter.eval_Stmt(node.false_stmt)
+	}
+}
 
+func (interpreter *ImpInterpreter) eval_WhileStmt(node WhileStmt) {
+	for true {
+		cond_val := interpreter.eval_Expr(node.cond)
+		cond_boolval, cond_is_bool := cond_val.(*BoolVal)
+		if !cond_is_bool {
+			panic(fmt.Sprintf("While statement got non-boolean condition '%s'\n", node.cond))
+		}
+		if cond_boolval.val == false {
+			break
+		}
+		interpreter.eval_Stmt(node.body_stmt)
+	}
+}
+
+func (interpreter *ImpInterpreter) eval_CallStmt(node CallStmt) {
+	interpreter.eval_function_call(node.func_name, node.args)
+}
+
+func (interpreter *ImpInterpreter) eval_PrintStmt(node PrintStmt) {
+	var outputs []string
+	for _, arg := range node.args {
+		arg_val := interpreter.eval_Expr(arg)
+		outputs = append(outputs, fmt.Sprintf("%s", arg_val))
+	}
+	fmt.Print(strings.Join(outputs, " "))
+}
+
+func (interpreter *ImpInterpreter) eval_ReturnStmt(node ReturnStmt) {
+	top_state := interpreter.get_top_state()
+	top_state.return_value = interpreter.eval_Expr(node.arg)
+	if top_state.current_function_name != "" {
+		expected_return_type := interpreter.Functions[top_state.current_function_name].Return_type
+		if !check_val_type_match(top_state.return_value, expected_return_type) {
+			panic(fmt.Sprintf("Function %s should return value of type %s, but actually returned '%s' of type %s\n", top_state.current_function_name, expected_return_type, node.arg, top_state.return_value))
+		}
+	}
+}
+
+func (interpreter *ImpInterpreter) eval_Stmt(nodes []Stmt) {
+	for _, stmt := range nodes {
+		switch stmt := stmt.(type) {
+		case *SkipStmt:
+			interpreter.eval_SkipStmt(*stmt)
+		case *AssignStmt:
+			interpreter.eval_AssignStmt(*stmt)
+		case *IfElseStmt:
+			interpreter.eval_IfElseStmt(*stmt)
+		case *WhileStmt:
+			interpreter.eval_WhileStmt(*stmt)
+		case *CallStmt:
+			interpreter.eval_CallStmt(*stmt)
+		case *PrintStmt:
+			interpreter.eval_PrintStmt(*stmt)
+		case *ReturnStmt:
+			interpreter.eval_ReturnStmt(*stmt)
+		}
+	}
 }
