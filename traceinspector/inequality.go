284e2a5df6be1d09de790689444eb314e3cb23d3
Dashadower <tttllshin@gmail.com>
2026-04-20 17:14:04 +0900

    Add simple integer inequality representation

---
+++ traceinspector/inequality.go
@@ -0,0 +1,113 @@
+package traceinspector
+
+import (
+	"fmt"
+	"traceinspector/imp"
+)
+
+// Represent an inequality of the form ±x ±y <= C and ±x <= C, where x and y are variables.
+// Also supports the form .
+type SimpleInequality struct {
+	x_varname string // variable name
+	x_is_pos  bool   // whether coefficient of x is positive
+	y_exists  bool   // whether the y variable exists
+	y_varname string // variable name
+	y_is_pos  bool   // whether coefficient of y is positive
+	constant  int    // constant value
+}
+
+func (ieq SimpleInequality) String() string {
+	var x_parity string
+	var y_parity string = "+"
+	if !ieq.x_is_pos {
+		x_parity = "-"
+	}
+	if !ieq.y_is_pos {
+		y_parity = "-"
+	}
+	if ieq.y_exists {
+		return fmt.Sprintf("%s%s %s %s <= %d", x_parity, ieq.x_varname, y_parity, ieq.y_varname, ieq.constant)
+	} else {
+		return fmt.Sprintf("%s%s <= %d", x_parity, ieq.x_varname, ieq.constant)
+	}
+}
+
+// Given an imp.Expr, check if the expr is of the form +-var. Only used within imp_expr_to_simp_inequality
+func _check_if_var(expr imp.Expr) (var_name string, is_negative bool) {
+	var_name = ""
+	is_negative = false
+	switch expr_ty := expr.(type) {
+	case *imp.NegExpr:
+		is_negative = true
+		var_name_res, is_negative_res := _check_if_var(expr_ty.Subexpr)
+		is_negative = is_negative != is_negative_res // xor
+		var_name = var_name_res
+	case *imp.VarExpr:
+		var_name = expr_ty.Name
+	}
+	return
+}
+
+// Given an imp leq expression, try and convert the expression into a SimpleInequality.
+// Returns SimpleInequality, and a boolean indicating whether the conversion was possible.
+// Very naive and lazy implementation btw
+func imp_expr_to_simp_inequality(expr imp.Expr) (SimpleInequality, bool) {
+	switch expr_ty := expr.(type) {
+	case *imp.LessthanExpr:
+		// convert to leq
+		// lhs < rhs -> lhs <= rhs - 1
+		return imp_expr_to_simp_inequality(&imp.LeqExpr{Node: expr_ty.Node, Lhs: expr_ty.Lhs, Rhs: &imp.SubExpr{Node: expr_ty.Node, Lhs: expr_ty.Rhs, Rhs: &imp.IntLitExpr{Node: expr_ty.Node, Value: 1}}})
+	case *imp.GreaterthanExpr:
+		// lhs > rhs -> rhs < lhs
+		return imp_expr_to_simp_inequality(&imp.LessthanExpr{Node: expr_ty.Node, Lhs: expr_ty.Rhs, Rhs: expr_ty.Lhs})
+	case *imp.GeqExpr:
+		// lhs >= rhs -> rhs <= lhs
+		return imp_expr_to_simp_inequality(&imp.LeqExpr{Node: expr_ty.Node, Lhs: expr_ty.Rhs, Rhs: expr_ty.Lhs})
+	case *imp.LeqExpr:
+		// move all terms to lhs
+		zero_expr, err := zero_rhs(expr)
+		if err != nil {
+			return SimpleInequality{}, false
+		}
+		zero_expr_leq, is_leq_expr := zero_expr.(*imp.LeqExpr)
+		if !is_leq_expr {
+			return SimpleInequality{}, false
+		}
+		// pull the constant out of LHS by representing LHS as polynomial
+		lhs_poly, err := build_polynomial(convert_subtraction_to_neg(zero_expr_leq.Lhs, false))
+		// fmt.Println(zero_expr_leq.Lhs, "||", lhs_poly.variable_expr, lhs_poly.constant)
+		created_ineq := SimpleInequality{}
+		created_ineq.constant = -lhs_poly.constant // sending it to other side of leq
+
+		// check if the polynomial is the form `±x + C`
+		single_varname, single_var_is_neg := _check_if_var(lhs_poly.variable_expr)
+		if single_varname != "" {
+			created_ineq.x_varname = single_varname
+			created_ineq.y_exists = false
+			created_ineq.x_is_pos = !single_var_is_neg
+			return created_ineq, true
+		}
+
+		// check if the polynomial is the form `±x ±y`
+		var x_varname, y_varname string
+		var x_is_neg, y_is_neg bool
+		switch poly_ty := lhs_poly.variable_expr.(type) {
+		case *imp.AddExpr:
+			x_varname, x_is_neg = _check_if_var(poly_ty.Lhs)
+			y_varname, y_is_neg = _check_if_var(poly_ty.Rhs)
+		case *imp.SubExpr:
+			x_varname, x_is_neg = _check_if_var(poly_ty.Lhs)
+			y_varname, y_is_neg = _check_if_var(poly_ty.Rhs)
+			y_is_neg = !y_is_neg
+		}
+		if x_varname != "" && y_varname != "" {
+			created_ineq.x_varname = x_varname
+			created_ineq.x_is_pos = !x_is_neg
+			created_ineq.y_exists = true
+			created_ineq.y_varname = y_varname
+			created_ineq.y_is_pos = !y_is_neg
+			return created_ineq, true
+		}
+	}
+	return SimpleInequality{}, false
+}
---
+++ traceinspector/inequality_test.go
@@ -0,0 +1,45 @@
+package traceinspector
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"path/filepath"
+	"testing"
+	"traceinspector/imp"
+)
+
+func Test_inequality(t *testing.T) {
+	test_go_files, err := filepath.Glob("test_files/*.go")
+	if err != nil {
+		panic(err)
+	}
+	for _, gofile := range test_go_files {
+		fset := token.NewFileSet()
+		// https://pkg.go.dev/go/parser#Mode
+		translator := imp.Go2ImpTranslator{Fset: fset}
+		runner := func(n ast.Node) bool {
+			switch expr := n.(type) {
+			case *ast.BinaryExpr:
+				switch expr.Op {
+				case token.EQL, token.NEQ, token.GEQ, token.LEQ, token.GTR, token.LSS:
+					original := translator.Translate_Expr(expr)
+					original, _ = zero_rhs(original)
+					new_expr, err := imp_expr_to_simp_inequality(original)
+					if err == true {
+						t.Logf("ineq: %s -> %s\n", original, new_expr)
+					}
+				}
+			}
+			return true
+		}
+
+		file, err := parser.ParseFile(fset, gofile, nil, parser.ParseComments)
+		if err != nil {
+			fmt.Println("Error while parsing", gofile, "-", err)
+			return
+		}
+		ast.Inspect(file, runner)
+	}
+}
