5d19a3bf30e6afe02afaa50a58191a8603b54257
Dashadower <tttllshin@gmail.com>
2026-04-20 21:24:37 +0900

    Implement some ops for interval

--- traceinspector/domain/interval.go
+++ traceinspector/domain/interval.go
@@ -2,90 +2,13 @@ package domain
 
 import (
 	"fmt"
-	"strconv"
+	"traceinspector/algebra"
 )
-
-// We don't use math.Inf since it results in a float
-type IntervalDomainValue struct {
-	value                  int
-	is_infty, is_neg_infty bool // disregard value field if any of these values is true
-}
-
-func (v IntervalDomainValue) String() string {
-	if v.is_infty {
-		return "∞"
-	} else if v.is_neg_infty {
-		return "-∞"
-	} else {
-		return strconv.Itoa(v.value)
-	}
-}
-
-// returns whether v is a finite value
-func (v IntervalDomainValue) Is_finite() bool {
-	return !(v.is_infty || v.is_neg_infty)
-}
-
-// Compute the minimum of two IntervalDomainValues
-func (l1 IntervalDomainValue) Min(l2 IntervalDomainValue) IntervalDomainValue {
-	if l1.is_neg_infty || l2.is_neg_infty {
-		return IntervalDomainValue{is_neg_infty: true} // zero values are 0 and false
-	} else if l1.is_infty && l2.is_infty {
-		return IntervalDomainValue{is_infty: true}
-	} else if l1.is_infty {
-		return IntervalDomainValue{value: l2.value}
-	} else if l2.is_infty {
-		return IntervalDomainValue{value: l1.value}
-	} else {
-		return IntervalDomainValue{value: min(l1.value, l2.value)}
-	}
-}
-
-// Compute the maximum of two IntervalDomainValues
-func (l1 IntervalDomainValue) Max(l2 IntervalDomainValue) IntervalDomainValue {
-	if l1.is_infty || l2.is_infty {
-		return IntervalDomainValue{is_infty: true}
-	} else if l1.is_neg_infty && l2.is_neg_infty {
-		return IntervalDomainValue{is_neg_infty: true}
-	} else if l1.is_neg_infty {
-		return IntervalDomainValue{value: l2.value}
-	} else if l2.is_neg_infty {
-		return IntervalDomainValue{value: l1.value}
-	} else {
-		return IntervalDomainValue{value: max(l1.value, l2.value)}
-	}
-}
-
-func (lhs IntervalDomainValue) Eq(rhs IntervalDomainValue) bool {
-	if lhs.is_infty {
-		return rhs.is_infty
-	} else if lhs.is_neg_infty {
-		return rhs.is_neg_infty
-	} else if rhs.is_infty || rhs.is_neg_infty {
-		return false
-	} else {
-		return lhs.value == rhs.value
-	}
-}
-
-func (lhs IntervalDomainValue) Leq(rhs IntervalDomainValue) bool {
-	if lhs.is_neg_infty {
-		return true
-	} else if rhs.is_infty { // lhs > -infty && rhs = infty
-		return true
-	} else if rhs.is_neg_infty { // rhs = -infty && lhs > -infty
-		return false
-	} else if lhs.is_infty { // lhs = infty && -infty < rhs < infty
-		return false
-	} else {
-		return lhs.value <= rhs.value
-	}
-}
 
 //////////////////////////////////
 
 type IntervalDomain struct {
-	lower, upper IntervalDomainValue
+	lower, upper algebra.ExtInt
 	is_bottom    bool
 }
 
@@ -97,16 +20,20 @@ func (domain IntervalDomain) String() string {
 	}
 }
 
+func IntervalBot() IntervalDomain {
+	return IntervalDomain{is_bottom: true}
+}
+
 func (domain IntervalDomain) IsBot() bool {
 	return domain.is_bottom
 }
 
 func (domain IntervalDomain) IsTop() bool {
-	return domain.lower.is_neg_infty && domain.upper.is_infty
+	return domain.lower.IsNegInfty() && domain.upper.IsInfty()
 }
 
 func (domain IntervalDomain) Is_bounded() bool {
-	return domain.lower.Is_finite() && domain.upper.Is_finite()
+	return domain.lower.IsFinite() && domain.upper.IsFinite()
 }
 
 func (lhs IntervalDomain) Join(rhs IntervalDomain) IntervalDomain {
@@ -129,18 +56,43 @@ func (lhs IntervalDomain) Widen(rhs IntervalDomain) IntervalDomain {
 		return lhs
 	}
 
-	var lower_val IntervalDomainValue
-	var upper_val IntervalDomainValue
+	var lower_val algebra.ExtInt
+	var upper_val algebra.ExtInt
 	if rhs.upper.Leq(lhs.upper) {
 		upper_val = rhs.upper
 	} else {
-		upper_val = IntervalDomainValue{is_infty: true}
+		upper_val = algebra.ExtInt_Infty()
 	}
 
 	if lhs.lower.Leq(rhs.lower) {
 		lower_val = lhs.lower
 	} else {
-		lower_val = IntervalDomainValue{is_neg_infty: true}
+		lower_val = algebra.ExtInt_NegInfty()
 	}
 	return IntervalDomain{lower: lower_val, upper: upper_val}
+}
+
+func (lhs IntervalDomain) Add(rhs IntervalDomain) IntervalDomain {
+	// [x1, x2] + [y1, y2] = [x1 + y1, x2 + y2]
+	if lhs.is_bottom || rhs.is_bottom {
+		return IntervalBot()
+	}
+	return IntervalDomain{lower: lhs.lower.Add(rhs.lower), upper: lhs.upper.Add(rhs.upper)}
+}
+
+func (lhs IntervalDomain) Sub(rhs IntervalDomain) IntervalDomain {
+	// [x1, x2] - [y1, y2] = [x1 - y1, x2 - y2]
+	if lhs.is_bottom || rhs.is_bottom {
+		return IntervalBot()
+	}
+	return IntervalDomain{lower: lhs.lower.Sub(rhs.lower), upper: lhs.upper.Sub(rhs.upper)}
+}
+
+func (lhs IntervalDomain) Mul(rhs IntervalDomain) IntervalDomain {
+	// [x1, x2] * [y1, y2] = [min(x1y1, x1y2, x2y1, x2y2) , max(x1y1, x1y2, x2y1, x2y2)]
+	x1y1 := lhs.lower.Mul(rhs.lower)
+	x1y2 := lhs.lower.Mul(rhs.upper)
+	x2y1 := lhs.upper.Mul(rhs.lower)
+	x2y2 := lhs.upper.Mul(rhs.upper)
+	return IntervalDomain{lower: x1y1.Min(x1y2, x2y1, x2y2), upper: x1y1.Max(x1y2, x2y1, x2y2)}
 }
