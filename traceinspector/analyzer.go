ee5ac240fd180f617771c431bfc805af244d00b7
Dashadower <tttllshin@gmail.com>
2026-05-13 18:07:27 +0900

    Implement post-fixpoint checks to determine dead branches

--- traceinspector/analyzer.go
+++ traceinspector/analyzer.go
@@ -403,6 +403,16 @@ func (interpreter *AbstractAnalyzer[IntDomainImpl, ArrayDomainImpl]) Step(in_sta
 		// We can just execute only the corresponding branch.
 		// Otherwise filter for each branch and join the result
 		cond_val := interpreter.Eval_expr(in_state, cfg_node.Cond_expr)
+
+		// cache the result of evaluating conditionals to detect dead branches later on
+		prev_condval, prev_condval_exists := interpreter.function_pre_mem_map[func_name].cond_node_condexpr_vals[in_state.node_location.Id]
+		if !prev_condval_exists {
+			interpreter.function_pre_mem_map[func_name].cond_node_condexpr_vals[in_state.node_location.Id] = cond_val.Get_bool()
+		} else {
+			new_condval, _ := prev_condval.Join(cond_val.Get_bool())
+			interpreter.function_pre_mem_map[func_name].cond_node_condexpr_vals[in_state.node_location.Id] = new_condval
+		}
+
 		// Try to represent it as SimpleProp
 		cond_simpleprop, simpleprop_success := algebra.Imp_expr_to_simple_prop(cfg_node.Cond_expr)
 		if !simpleprop_success {
@@ -499,6 +509,27 @@ func (analyzer *AbstractAnalyzer[IntDomainImpl, ArrayDomainImpl]) Interpret_func
 	// fmt.Println("Final mem", analyzer.function_pre_mem_map[function_name])
 }
 
+func (analyzer *AbstractAnalyzer[IntDomainImpl, ArrayDomainImpl]) Run_post_checks() {
+	for func_name, cfg := range analyzer.Function_cfgs {
+		for node_id, node := range cfg.Node_map {
+			// check for always true or always false branches, or bot(uncomputable)
+			cond_node, is_cond_node := node.(*CFGCondNode)
+			if is_cond_node {
+				node_eval_info, info_exists := analyzer.function_pre_mem_map[func_name].cond_node_condexpr_vals[node_id]
+				if info_exists {
+					if node_eval_info.IsBot() {
+						analyzer.output_handler.write_warning(cond_node.Loc, fmt.Sprintf("Possible dead branch: boolean expression '%s' evaluates to ⊥", cond_node.Code))
+					} else if node_eval_info.IsFalse() {
+						analyzer.output_handler.write_warning(cond_node.Loc, fmt.Sprintf("Possible dead branch: boolean expression '%s' always evaluates to false", cond_node.Code))
+					} else if node_eval_info.IsTrue() {
+						analyzer.output_handler.write_warning(cond_node.Loc, fmt.Sprintf("Possible dead branch: boolean expression '%s' always evaluates to true", cond_node.Code))
+					}
+				}
+			}
+		}
+	}
+}
+
 func Test(func_cfg_map FunctionCFGMap, func_name imp.ImpFunctionName, func_info_map imp.ImpFunctionMap, debug bool) {
 	g := AbstractAnalyzer[domain.IntervalDomain, ArraySummaryDomain[domain.IntervalDomain]]{
 		Function_cfgs: func_cfg_map,
@@ -513,6 +544,7 @@ func Test(func_cfg_map FunctionCFGMap, func_name imp.ImpFunctionName, func_info_
 	}
 	g.Initialize()
 	g.Run_analysis()
+	g.Run_post_checks()
 	g.output_handler.Print()
 	if !debug {
 		return
